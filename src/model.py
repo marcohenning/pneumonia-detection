8c7d728223ef9cd00616471f71927984b892c08d
Marco Henning <marcohnngpersonal@gmail.com>
2024-10-31 23:59:27 +0100

    Import model from my other repo as starting point

---
+++ src/model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(128 * 16 * 16, 256)
+        self.fc2 = nn.Linear(256, 10)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 16 * 16)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
