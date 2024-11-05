defd0c90df2c6af943b1a04d7c4142a4a8280db3
Marco Henning <marcohnngpersonal@gmail.com>
2024-11-05 19:19:46 +0100

    Start training neural network

--- src/train.py
+++ src/train.py
@@ -1,5 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
+from model import Model
 from dataset import PneumoniaDataset
 
 
@@ -17,3 +21,35 @@ testing_split = len(dataset) - training_split
 training_data, testing_data = random_split(dataset, [training_split, testing_split])
 training_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 testing_dataloader = DataLoader(testing_data, batch_size=batch_size, shuffle=True)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = Model().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+losses = []
+
+for i in range(10):
+    for images, labels in training_dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        predictions = model(images)
+        loss = criterion(predictions, labels)
+        loss.backward()
+        optimizer.step()
+    losses.append(loss.item())
+    print(loss.item())
+
+correct = 0
+model.eval()
+with torch.no_grad():
+    for images, labels in testing_dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        predictions = model(images)
+        _, predicted = torch.max(predictions.data, 1)
+        correct += (predicted == labels).sum().item()
+
+print(correct / testing_split * 100)
